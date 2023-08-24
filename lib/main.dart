f1841ddb769818b7c8e373adbba94a6dc0ad80c3
Arpeeee <brian9609@example.com>
2023-08-24 23:02:49 +0800

    Feat:穩定版本1.0.0
    調整項目:
    1.修改chart樣式與顏色
    2.修改畫圖邏輯

--- lib/main.dart
+++ lib/main.dart
@@ -71,16 +71,27 @@ class _LineChartPageState extends State<LineChartPage> {
 
   // 計算series
   void startUpdatingData() {
-    timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
-      if (dataPoints.length < 200) {
+    timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      double barvalue =
+          1 + (_temvalue / 10000) - (_fanvalue / 20000) - (_tongvalue / 20000);
+      if (dataPoints.length <= 92) {
         // 負斜率
-        dataPoints.add(FlSpot(
-            keyfl, (_fanvalue / 10) + (_temvalue / 10) - (keyfl / 1000)));
+
+        keyfl != 0
+            ? dataPoints.add(FlSpot(
+                keyfl,
+                Random().nextDouble() * 0.3 +
+                    (barvalue - 0.01) * (dataPoints[(keyfl - 1).toInt()].y)))
+            : dataPoints.add(const FlSpot(0, 40));
         keyfl++;
-      } else if (dataPoints.length < 4000) {
+      } else if (dataPoints.length > 92 && dataPoints.length < 200) {
         // 正斜率
         dataPoints.add(FlSpot(
-            keyfl, (_fanvalue / 10) + (_temvalue / 10) + (keyfl / 1000)));
+            keyfl,
+            -Random().nextDouble() * 0.1 +
+                0.05 +
+                (keyfl / 10000) +
+                (barvalue * dataPoints[(keyfl - 1).toInt()].y)));
         keyfl++;
       } else {
         // 超過時間就停止
@@ -101,58 +112,105 @@ class _LineChartPageState extends State<LineChartPage> {
 
   void _toggleCook() {
     // 如果沒再轟
-    _isStart.value ? _isStart.value = false : _isStart.value = true;
+    _isStart.value ? _endCook() : _startCook();
     setState(() {});
   }
 
-  void _start() {
-    _isStart.value = true;
-    setState(() {});
+  void _startCook() {
+    if (_isStart.value) {
+      showAlertDialog(
+        context,
+        "錯誤",
+        "已下豆 正在烘豆中",
+        () => Navigator.pop(context),
+        () => Navigator.pop(context),
+      );
+    } else {
+      showAlertDialog(
+        context,
+        "警告",
+        "請確認是否下豆",
+        () {
+          setState(() {
+            _isStart.value = true;
+            Navigator.pop(context);
+          });
+        },
+        () {
+          Navigator.pop(context);
+        },
+      );
+    }
   }
 
-  void _end() {
-    _isStart.value = false;
-    setState(() {});
+  void _endCook() {
+    if (!_isStart.value) {
+      showAlertDialog(
+        context,
+        "錯誤",
+        "已出豆或未開始烘豆",
+        () => Navigator.pop(context),
+        () => Navigator.pop(context),
+      );
+    } else {
+      showAlertDialog(
+        context,
+        "警告",
+        "請確認是否出豆",
+        () {
+          setState(() {
+            _isStart.value = false;
+            Navigator.pop(context);
+          });
+        },
+        () {
+          Navigator.pop(context);
+        },
+      );
+    }
   }
 
   // 框框
   Widget _borderPadding(Widget childWidget) {
     return Padding(
-        padding: EdgeInsets.all(10),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 20, color: Color.fromRGBO(36, 43, 60, 1)),
-            borderRadius: BorderRadius.circular(20),
-            // color: Color.fromRGBO(36, 43, 60, 1),
-          ),
-          child: childWidget,
-        ));
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        decoration: BoxDecoration(
+          border:
+              Border.all(width: 20, color: const Color.fromRGBO(36, 43, 60, 1)),
+          borderRadius: BorderRadius.circular(20),
+          // color: Color.fromRGBO(36, 43, 60, 1),
+        ),
+        child: childWidget,
+      ),
+    );
   }
 
   // 出豆下豆按鈕
-  Widget _beansBtn(
-      String activeText, IconData icon, bool isactive, Function onpress) {
+  Widget _beansBtn(String activeText, IconData icon, bool isactive,
+      void Function() onpress) {
     return Container(
       margin: const EdgeInsets.all(10),
       height: double.infinity,
       child: ElevatedButton(
-          onPressed: () => onpress,
-          style: const ButtonStyle(
-              backgroundColor:
-                  MaterialStatePropertyAll(Color.fromRGBO(248, 167, 69, 1))),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                activeText,
-                style: const TextStyle(color: Colors.black87, fontSize: 25),
-              ),
-              Icon(
-                icon,
-                color: Colors.black87,
-              )
-            ],
-          )),
+        onPressed: onpress,
+        style: const ButtonStyle(
+            backgroundColor:
+                MaterialStatePropertyAll(Color.fromRGBO(248, 167, 69, 1))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              activeText,
+              style: const TextStyle(color: Colors.black87, fontSize: 25),
+            ),
+            Icon(
+              icon,
+              color: Colors.black87,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -160,19 +218,30 @@ class _LineChartPageState extends State<LineChartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated Line Chart'),
-        backgroundColor: Color.fromRGBO(255, 76, 0, 1),
+        title: const Text(
+          '瑞提娜咖啡機',
+          style: TextStyle(fontWeight: FontWeight.w600, letterSpacing: 5),
+        ),
+        backgroundColor: const Color.fromRGBO(255, 76, 0, 1),
       ),
       //
       body: Row(
         children: [
           SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: double.infinity,
-              child: _borderPadding(Padding(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: double.infinity,
+            child: _borderPadding(
+              Container(
                 padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("images/Retina.png"),
+                        opacity: 0.7,
+                        fit: BoxFit.scaleDown)),
                 child: MyLineWidget(value: dataPoints),
-              ))),
+              ),
+            ),
+          ),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.3,
             height: double.infinity,
@@ -183,26 +252,26 @@ class _LineChartPageState extends State<LineChartPage> {
                     child: Row(
                       children: [
                         Expanded(
-                            flex: 1,
-                            child: SliderCustom(
-                              title: "风扇转速",
-                              value: 0.0,
-                              callback: (newvalue) => {_fanvalue = newvalue},
-                            )),
+                          flex: 1,
+                          child: SliderCustom(
+                            title: "风扇转速",
+                            callback: (newvalue) => {_fanvalue = newvalue},
+                          ),
+                        ),
                         Expanded(
-                            flex: 1,
-                            child: SliderCustom(
-                              title: "瓦斯火力",
-                              value: 0.0,
-                              callback: (newvalue) => {_temvalue = newvalue},
-                            )),
+                          flex: 1,
+                          child: SliderCustom(
+                            title: "瓦斯火力",
+                            callback: (newvalue) => {_temvalue = newvalue},
+                          ),
+                        ),
                         Expanded(
-                            flex: 1,
-                            child: SliderCustom(
-                              title: "滚筒转速",
-                              value: 0.0,
-                              callback: (newvalue) => {_temvalue = newvalue},
-                            ))
+                          flex: 1,
+                          child: SliderCustom(
+                            title: "滚筒转速",
+                            callback: (newvalue) => {_tongvalue = newvalue},
+                          ),
+                        )
                       ],
                     )),
                 Expanded(
@@ -210,20 +279,17 @@ class _LineChartPageState extends State<LineChartPage> {
                     child: Row(
                       children: [
                         Expanded(
-                            child: _beansBtn("下豆", Icons.filter_alt, true, () {
-                          setState(() {
-                            _isStart.value = true;
-                          });
-                        })),
+                            child: _beansBtn(
+                                "入豆", Icons.filter_alt, true, _startCook)),
                         Expanded(
-                            child: _beansBtn("出豆", Icons.coffee_outlined, false,
-                                () => _end()))
+                            child: _beansBtn(
+                                "出豆", Icons.coffee_outlined, false, _endCook))
                       ],
                     )),
                 Expanded(
                     flex: 1,
                     child: Container(
-                        padding: EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(10),
                         width: double.infinity,
                         height: double.infinity,
                         child: TextButton(
@@ -231,8 +297,8 @@ class _LineChartPageState extends State<LineChartPage> {
                           style: ButtonStyle(
                               backgroundColor: MaterialStatePropertyAll(
                                   !_isStart.value
-                                      ? Color.fromRGBO(155, 0, 0, 1)
-                                      : Color.fromRGBO(176, 43, 59, 0.8)),
+                                      ? const Color.fromRGBO(155, 0, 0, 1)
+                                      : const Color.fromRGBO(176, 43, 59, 0.8)),
                               padding: const MaterialStatePropertyAll(
                                   EdgeInsets.all(0))),
                           child: Text(
@@ -256,15 +322,16 @@ class _LineChartPageState extends State<LineChartPage> {
 // slider
 class SliderCustom extends StatefulWidget {
   // const sliderCustom({super.key});
-  const SliderCustom(
-      {super.key,
-      required this.callback,
-      this.title = "測試",
-      required this.value});
+  const SliderCustom({
+    super.key,
+    required this.callback,
+    this.title = "測試",
+    this.value = 50,
+  });
 
   final void Function(double newvalue) callback;
   final String title;
-  final dynamic value;
+  final double value;
 
   @override
   State<SliderCustom> createState() => _SliderCustomState();
@@ -272,14 +339,13 @@ class SliderCustom extends StatefulWidget {
 
 class _SliderCustomState extends State<SliderCustom> {
   // dynamic _value get  => widget.value;
-  get _title => widget.title;
-  dynamic _value2;
+  late String _title;
+  late double _value2;
 
   @override
   void initState() {
     super.initState();
-
-    // TODO: implement initState
+    _title = widget.title;
     _value2 = widget.value;
   }
 
@@ -307,7 +373,7 @@ class _SliderCustomState extends State<SliderCustom> {
               numberFormat: NumberFormat(""),
               // showDividers: true,
               minorTicksPerInterval: 1,
-              // onChangeStart: (value) => _value,
+
               onChanged: (dynamic value) async {
                 _value2 = value;
                 widget.callback(value);
@@ -329,20 +395,6 @@ class MyLineWidget extends StatefulWidget {
 }
 
 class _MyLineWidgetState extends State<MyLineWidget> {
-  List<double> list = [1, 2, 4, 6, 7, 3, 2, 6, 2];
-
-  LineChartData get sampleData2 => LineChartData(
-        lineTouchData: lineTouchData2,
-        gridData: FlGridData(show: true),
-        titlesData: titlesData2,
-        borderData: borderData,
-        lineBarsData: [lineBarsData2],
-        minX: 0,
-        maxX: 5000,
-        maxY: 20,
-        minY: 0,
-      );
-
   FlBorderData get borderData => FlBorderData(
         show: true,
         border: Border.all(
@@ -359,14 +411,12 @@ class _MyLineWidgetState extends State<MyLineWidget> {
       );
 
   FlTitlesData get titlesData2 => FlTitlesData(
-        show: false,
+        show: true,
         bottomTitles: AxisTitles(
           sideTitles: SideTitles(
             getTitlesWidget: (value, meta) {
               return const Text(
                 "123",
-                style: TextStyle(),
-                textAlign: TextAlign.center,
               );
             },
           ),
@@ -378,24 +428,29 @@ class _MyLineWidgetState extends State<MyLineWidget> {
           sideTitles: SideTitles(showTitles: false),
         ),
         leftTitles: AxisTitles(
-          sideTitles: SideTitles(
-            getTitlesWidget: (value, meta) {
-              return const Text(
-                "123",
-                style: TextStyle(color: Colors.black, fontSize: 20),
-                textAlign: TextAlign.center,
-              );
-            },
-          ),
+          sideTitles: SideTitles(showTitles: false),
         ),
       );
 
+  LineChartData get sampleData2 => LineChartData(
+        lineTouchData: lineTouchData2,
+        gridData: FlGridData(show: true),
+        titlesData: titlesData2,
+        borderData: borderData,
+        lineBarsData: [lineBarsData2],
+        minX: 0,
+        maxX: 1500,
+        maxY: 100,
+        minY: 0,
+      );
   LineChartBarData get lineBarsData2 => LineChartBarData(
+        show: widget.value!.isNotEmpty,
         spots: widget.value,
         isCurved: true,
-        color: Colors.blue,
+        color: Color.fromARGB(255, 243, 68, 33),
+        barWidth: 5,
         dotData: FlDotData(show: false),
-        belowBarData: BarAreaData(show: true),
+        // belowBarData: BarAreaData(show: true),
       );
 
   @override
@@ -408,52 +463,20 @@ class _MyLineWidgetState extends State<MyLineWidget> {
   }
 }
 
-class ShowDialogWidget extends StatelessWidget {
-  const ShowDialogWidget(
-      {super.key,
-      required this.okOnPress,
-      required this.cancelOnPress,
-      required this.message});
-  final void okOnPress;
-  final void cancelOnPress;
-  final String message;
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      actionsAlignment: MainAxisAlignment.center,
-      title: Text(message),
-      actions: [
-        ElevatedButton(
-            child: const Text("OK"),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-        ElevatedButton(
-            child: const Text("Cancel"),
-            onPressed: () {
-              Navigator.pop(context);
-            }),
-      ],
-    );
-  }
-}
-
-showAlertDialog(BuildContext context) {
+void showAlertDialog(
+  BuildContext context,
+  String title,
+  String content,
+  void Function() yesbutton,
+  void Function() nobutton,
+) {
   // Init
   AlertDialog dialog = AlertDialog(
-    title: Text("AlertDialog component"),
+    title: Text(title),
+    content: Text(content),
     actions: [
-      ElevatedButton(
-          child: Text("OK"),
-          onPressed: () {
-            Navigator.pop(context);
-          }),
-      ElevatedButton(
-          child: Text("Cancel"),
-          onPressed: () {
-            Navigator.pop(context);
-          }),
+      ElevatedButton(child: const Text("OK"), onPressed: yesbutton),
+      ElevatedButton(child: const Text("Cancel"), onPressed: nobutton),
     ],
   );
 
